dd30048937648a665bcd446cae4a03a28ca8bfd2
MilanAjtic <milanajtic@gmail.com>
2018-10-10 00:28:34 +0200

    prvi dodatak

---
+++ src/Footer.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class Footer extends Component {
+  render() {
+    const datum = new Date();
+    const ime = "Milan";
+    // ovde moze cist javascript, dole moze samo podskup
+    // dole bi mogli izrazi, ne bi mogle naredbe
+    return (
+      <footer>
+        <p>Zdravo korisnice: {ime}</p>
+        Made by: Skola koda <br />
+        {datum.toLocaleDateString()}
+      </footer>
+    );
+  }
+}
+
+export default Footer;
